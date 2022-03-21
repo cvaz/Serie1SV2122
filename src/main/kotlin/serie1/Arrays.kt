c71d65793062c947f47c3308a20e933e2d3d03fc
cvaz <cvaz@cc.isel.ipl.pt>
2022-03-21 12:23:04 +0000

    Initial commit

---
+++ src/main/kotlin/serie1/Arrays.kt
@@ -0,0 +1,19 @@
+package serie1
+
+data class Point(var x:Int, var y:Int)
+
+fun upperBound(a: IntArray, l: Int, r: Int, element: Int): Int {
+    throw UnsupportedOperationException()
+}
+
+fun countIncreasingSubArrays(arr: IntArray): Int {
+    throw UnsupportedOperationException()
+}
+
+fun countEquals(points1: Array<Point>, points2: Array<Point>, cmp: (p1:Point, p2:Point)-> Int): Int {
+    throw UnsupportedOperationException()
+}
+
+fun mostLonely(a: IntArray): Int {
+    throw UnsupportedOperationException()
+}
